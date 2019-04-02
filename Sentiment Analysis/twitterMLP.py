a7943f79478320cacbc2ebc1a94f3b7e865e8639
Yojouhan <49159419+Yojouhan@users.noreply.github.com>
2019-04-02 22:39:03 +0300

    Fixed some comments

--- Sentiment Analysis/twitterMLP.py
+++ Sentiment Analysis/twitterMLP.py
@@ -29,8 +29,8 @@ with open('data/twitter/train_set.csv', 'r', encoding='utf8') as trainfile:
         # Append tweet to corpus list
         corpus.append(row[-1])
         # Append sentiment to sentiments list
-        # 0 is negative, 2 is neutral and 4 is positive => mapped to 0, 1 and 2
-        # This is a multiclass problem.
+        # 0 is negative,  4 is positive => mapped to 0 and 1
+        # This is a binary problem.
         if row[0] == '0':
             sentiments.append(0)
         elif row[0] == '4':
@@ -50,7 +50,7 @@ print(Counter(sentiments))
 
 
 # Function to clean the corpus. Twitter data is very "dirty", and contains a lot of irrelevant info which can affect
-# accuracy signifcantly
+# accuracy signifcantly. Will keep modifying as further tests are performed.
 def preprocess(texts):
     # Remove http links
     cleanCorpus = []
